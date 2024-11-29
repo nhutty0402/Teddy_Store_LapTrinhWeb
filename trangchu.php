898f9ad0013831a5f46ff4d894f498c430fbbeaa
nhutty0402 <nhutty04022021@gmail.com>
2024-11-29 21:03:14 +0700

    Create trangchu.php

---
+++ trangchu.php
@@ -0,0 +1,51 @@
+
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhà Trọ Cần Thơ</title>
+    <link href="/css/trangchu.css" rel="stylesheet" />
+<!-- Custom Trang Chủ -->
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" /> <!-- Bootstrap -->
+    <link href="css/style.css" rel="stylesheet" /> <!-- Custom Styles -->
+    <link href="css/owl.carousel.min.css" rel="stylesheet" /> <!-- Owl Carousel -->
+    <link href="css/all.min.css" rel="stylesheet" /> <!-- Font Awesome -->
+   
+
+    <!-- JS -->
+    <script src="js/jquery.min.js"></script> <!-- jQuery -->
+    <script src="js/popper.min.js"></script> <!-- Popper.js -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap JS -->
+    <script src="js/s3.js"></script> <!-- Custom JS -->
+    <script src="js/all.min.js"></script> <!-- Font Awesome JS -->
+    <script src="js/myscript.js"></script> <!-- Additional Custom JS -->
+</head>
+    
+<body>
+<!-- Thanh công cụ ngang (Navbar) -->
+
+<div class="header-bar">
+    <h1 class="cute-title gradient-text">Chào mừng đến với Gấu Bông CTUT</h1>
+    <div class="search-bar">
+        <input type="text" placeholder="Tìm kiếm..." />
+        <button type="submit">🔍</button>
+    </div>
+</div>
+
+
+<div class="navbar">
+    <a href="home.html">Trang chủ</a>
+    <a href="khach.html">sản phẩm</a>
+    <a href="DTmoi.html">Doanh thu</a>
+  
+</div>
+
+
+
+</body>
+</html>
